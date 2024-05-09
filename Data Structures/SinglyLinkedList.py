8310e1a02746cfbbb45ec12d060fcb2b96db19d5
DragonKrissash <dragonkrishoredbzgt@gmail.com>
2024-05-09 21:31:47 +0530

    9-5-2024

---
+++ Data Structures/SinglyLinkedList.py
@@ -0,0 +1,113 @@
+class Node:
+
+    def __init__(self,data,next=None):
+        self.data=data
+        self.next=next
+
+    def __str__(self):
+        return str(self.data)
+
+class SLL:
+
+    def __init__(self):
+        self.__head=None
+        self.__tail=None
+        self.__size=0
+
+    def size(self):
+        return self.__size
+
+    def isEmpty(self):
+        return self.__size==0
+
+    def __str__(self):
+        l=[]
+        te=self.__head
+        while te is not None:
+            l.append(te.data)
+            te=te.next
+        return '->'.join(map(str,l))
+
+    def append(self,data):
+        node = Node(data)
+        if self.isEmpty():
+            self.__head=node
+            self.__tail=node
+        else:
+            self.__tail.next=node
+            self.__tail=node
+        self.__size+=1
+
+    def addFirst(self,data):
+        node=Node(data)
+        if self.isEmpty():
+            self.append(data)
+        else:
+            node.next=self.__head
+            self.__head=node
+        self.__size+=1
+
+    def addAt(self,ind,data):
+        if self.isEmpty():
+            raise Exception('Empty Linked List')
+        if ind<0 or ind >self.__size:
+            raise Exception('Index out of bound!')
+        elif ind==0:
+            self.addFirst(data)
+        elif ind==self.__size:
+            self.append(data)
+        else:
+            temp=self.__head
+            id=0
+            while id+1 != ind:
+                id+=1
+                temp=temp.next
+            node=Node(data,temp.next)
+            temp.next=node
+        self.__size+=1
+
+    def removeFirst(self):
+        if self.isEmpty():
+            raise Exception("Empty Linked List")
+        temp=self.__head
+        self.__head=self.__head.next
+        del(temp)
+        self.__size-=1
+
+    def removeLast(self):
+        if self.isEmpty():
+            raise Exception('Empty Linked List')
+        temp=self.__head
+        while temp.next is not self.__tail:
+            temp=temp.next
+        self.__tail=temp
+        self.__tail.next=None
+        temp=temp.next
+        del(temp)
+        self.__size-=1
+
+    def removeAt(self,ind):
+        if self.isEmpty():
+            raise Exception('Empty Linked List')
+        if ind==0:
+            self.removeFirst()
+        elif ind==self.__size-1:
+            self.removeLast()
+        else:
+            temp=self.__head
+            id=0
+            while id<ind-1:
+                temp=temp.next
+                id+=1
+            te=temp.next
+            temp.next= temp.next.next
+            del(te)
+            self.__size-=1
+
+sll=SLL()
+sll.append(4)
+sll.append(5)
+sll.append(10)
+print(sll)
+sll.removeAt(2)
+print(sll)
