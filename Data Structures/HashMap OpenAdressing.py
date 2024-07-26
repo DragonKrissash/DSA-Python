aa489be57c63c8c445af65fab467efaab9f5318a
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-07-26 19:21:34 +0530

    26-7-2024

--- Data Structures/HashMap OpenAdressing.py
+++ Data Structures/HashMap OpenAdressing.py
@@ -23,21 +23,40 @@ class HT:
         entry=Entry(key,value)
         index=entry.hash % self.cap
         i=0
-        print(f'Inserting key {str(key)}')
         while True:
             ind=(index+self.prob(i)) % self.cap
-            print(f'Trying index: {str(ind)}')
             if self.data[ind]==None:
                 self.data[ind]=entry
-                print(f'Inserting at index : {ind}')
                 break
             i+=1
 
-    def print(self):
+    def find(self,key):
+        index=hash(key)%self.cap
         for i in range(self.cap):
-            print(self.data[i],sep=',')
+            ind=(index+self.prob(i))%self.cap
+            if self.data[ind] is not None:
+                return True
+        return False
+
+    def remove(self,key):
+        if self.find(key):
+            index=hash(key)%self.cap
+            for i in range(self.cap):
+                ind=(index+self.prob(i))%self.cap
+                if self.data[ind] is not None and self.data[ind].key==key:
+                    self.data[ind]=None
+                    return True
+        else:
+            return False
+
+    def __str__(self):
+        for i in range(self.cap):
+            print(self.data[i])
+        return ""
 
 ht=HT()
 ht.insert('roll 1',123)
 ht.insert('roll 2',432)
-ht.print()
+ht.insert('2',1)
+print(ht.remove('2'))
+print(ht)
